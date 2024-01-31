30755428a19bd7ea73012019b8ec00571bc7204f
Andre <andrepontde@gmail.com>
2024-01-31 10:37:24 +0000

    Commented code

--- HotelBooking.java
+++ HotelBooking.java
@@ -1,11 +1,21 @@
+/**
+	Authors: André Pont, Jordan Carthy.
+	Hotelbooking.Java
+	31/01/2024
+**/
+
 
 import java.util.Scanner;
 public class HotelBooking{
 public static void main (String []args){
 
+//First we call the user input as a Scanner for us to use it later.
+
 Scanner userInput = new Scanner(System.in);
 
 System.out.println("How many people wish to stay :");
+
+//We declare the type of data and the variable to be able to multiply it later.
 
 int amountPeople = userInput.nextInt();
 
@@ -14,6 +24,8 @@ System.out.println("How many nights do you wish to stay :");
 int amountNights = userInput.nextInt();
 
  int totalAmount = ((amountPeople * amountNights)*60);
+
+//We format the output this way so we dont have to concatenate manually.
 
  System.out.printf("The final amount is equal to : %d \n", totalAmount);
 
--- Teams.java
+++ Teams.java
@@ -4,12 +4,14 @@
 	23/01/2024
 **/
 import java.util.Scanner;
+//We import the scan function.
 
 public class Teams{
 	public static void main (String args[]){
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please input the number of students");
 		int students = userInput.nextInt();
+		//We divide the user input between 5 so we have another variable with the result.
 		int studentsDivided = students/5;
 		System.out.printf("The number of students is %d and the number of possible teams is: %d \n", students, studentsDivided);
 	}
